7edbb1212cf51595f12a80395557690129c1df29
dbedollac <daniel.bedollac@gmail.com>
2022-05-06 22:10:19 -0500

    Adding function to guess the values of p and q basedn on the ACF and the PACF

--- boxjenkins/raw_functions.py
+++ boxjenkins/raw_functions.py
@@ -1,6 +1,8 @@
 from scipy.optimize import minimize
 import numpy as np
 from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import acf
+from statsmodels.tsa.stattools import pacf
 
 def BoxCox(x,l:int):
     """
@@ -102,3 +104,151 @@ def DickeyD(x_array):
         adf_t = adfuller(np.diff(x_array,n=d))
         pvalue = adf_t[1]
     return d
+
+def ComparePolyRoots(x_coeffs,y_coeffs,epsilon=0.2):
+    """
+    This function compares the real roots of 2 polynomials and returns True if some pair of roots can be considered equal given an epsilon.
+
+    Parameters
+    ----------
+    x_coeffs: ndarray
+        Array with the coefficients of the first polynomial.
+
+    y_coeffs: ndarray
+        Array with the coefficients of the second polynomial.
+
+    Returns
+    -------
+    test: bool
+        True if there is a common root, False otherwise.
+    """
+
+    x_roots = np.roots(x_coeffs)
+    x_real_roots = x_roots[np.isreal(x_roots)]
+
+    y_roots = np.roots(y_coeffs)
+    y_real_roots = y_roots[np.isreal(y_roots)]
+
+    test = False
+    for x_r in list(x_real_roots):
+        if test == True:
+            break
+        for y_r in list(y_real_roots):
+            if abs(x_r-y_r) <= epsilon:
+                test = True
+                break
+    return test
+
+def MaxSigACFlag(x_array):
+    """
+    This function assess the lags of the ACF and find the last k_lag significantly distinct of 0 with alpha ~5%. The standard deviation is computed according to Bartlett's formula.
+
+    Parameters
+    ----------
+    x_coeffs: ndarray
+        Array with the observed values of the time series.
+
+    Returns
+    -------
+    q_lag: int
+        The last k_lag significantly distinct of 0.
+
+    q_tail: int
+        Number of k_lags significantly distinct of 0.
+
+    acf_std: list
+        List of the standard deviations of lag_k when q = k-1, beginning with k = 1.
+    """
+    x_acf = acf(x_array, alpha=0.05)
+
+    x_acf_std = [(l[1] - l[0]) / 4 for l in list(x_acf[1])] # Based on the CI computed by statsmodels that uses 2 standard deviations around each value of the ACF function.
+
+    try:
+        q_lag = np.max(np.where(abs(x_acf[0]) > 2*np.array(x_acf_std)))
+        q_tail = sum(abs(x_acf[0]) > 2*np.array(x_acf_std))-1
+    except:
+        q_lag = np.inf
+        q_tail = np.inf
+
+    return {'q_lag':q_lag, 'acf_std':x_acf_std[1:], 'q_tail':q_tail}
+
+def MaxSigPACFlag(x_array):
+    """
+    This function assess the lags of the PACF and find the last k_lag significantly distinct of 0 with alpha ~5%. The standard deviation is computed according to 1/sqrt(len(x)).
+
+    Parameters
+    ----------
+    x_coeffs: ndarray
+        Array with the observed values of the time series.
+
+    Returns
+    -------
+    p_lag: int
+        The last k_lag significantly distinct of 0.
+
+    p_tail: int
+        Number of k_lags significantly distinct of 0.
+
+    pacf_std: float
+        Standard deviations for every lag in the PACF.
+    """
+    x_pacf = pacf(x_array, alpha=0.05)
+    x_pacf_std = 1/(len(x_array))**0.5
+    try:
+        p_lag = np.max(np.where(abs(x_pacf[0]) > 2*x_pacf_std))
+        p_tail = sum(abs(x_pacf[0]) > 2*x_pacf_std)-1
+    except:
+        p_lag = np.inf
+        p_tail = np.inf
+
+    return {'p_lag':p_lag, 'pacf_std':x_pacf_std, 'p_tail':p_tail}
+
+
+def GuessModel(x_array):
+    """
+    This function guess the value of the parameters p and q of an ARIMA model based on the strategy developed by Box and Jenkis, which depends on the interpretation of the ACF and the PACF.
+
+    Parameters
+    ----------
+    x_coeffs: ndarray
+        Array with the observed values of the time series.
+
+    Returns
+    -------
+    p: int
+        The proposed parameter for the p-parameter.
+    q: int
+        The proposed parameter for the q-parameter.
+    """
+    max_p = MaxSigPACFlag(x_array)
+    max_q = MaxSigACFlag(x_array)
+
+    x_acf = acf(x_array)
+    x_acf_diff = abs(np.diff(abs(x_acf),n=1))
+    test_acf = x_acf_diff > max_q['acf_std']
+    try:
+        min_q = np.where(test_acf == True)[0][0] + 1
+    except:
+        min_q = np.inf
+
+    x_pacf = pacf(x_array)
+    x_pacf_diff = abs(np.diff(abs(x_pacf),n=1))
+    test_pacf = x_pacf_diff > max_p['pacf_std']
+
+    try:
+        min_p = np.where(test_pacf == True)[0][0] + 1
+    except:
+        min_p = np.inf
+
+    if max_q['q_tail'] > max_p['p_tail']:
+        p = max_p['p_lag']
+        q = 0
+    else:
+        if max_q['q_lag'] < min_q+min_p:
+            q = max_q['q_lag']
+            p = 0
+        else:
+            p = min_p
+            q = min_q
+
+    return {'p':p,'q':q}
--- tests/test_igae_raw.py
+++ tests/test_igae_raw.py
@@ -1,5 +1,8 @@
 from boxjenkins.raw_functions import *
 import pandas as pd
+from statsmodels.tsa.stattools import acf
+from statsmodels.tsa.stattools import pacf
+import matplotlib.pyplot as plt
 
 # Reading data
 df_inpc = pd.read_csv('../data/inp_guerrero.csv')
@@ -20,3 +23,41 @@ print('anderson_d',anderson_d)
 # Selecting d wiht Dickey Fuller tests
 dickey_d = DickeyD(df_inpc.ipc_t)
 print('dickey_d',dickey_d)
+
+# Testing polynomials wiht similar roots
+x_coeffs = np.array([1,-3.01,2])
+y_coeffs = np.array([1,-3,2])
+poly_test = ComparePolyRoots(x_coeffs,y_coeffs,epsilon=0.01)
+print('common roots',poly_test)
+
+# Testing to find the maximum lags of the ACF and PAC significantly distinct of 0
+df_inpc['ipc_t_diff'] = df_inpc['ipc_t'].diff()
+max_q = MaxSigACFlag(df_inpc['ipc_t_diff'].dropna())
+max_p = MaxSigPACFlag(df_inpc['ipc_t_diff'].dropna())
+print('max_q',max_q['q_lag'])
+print('acf_std',max_q['acf_std'])
+print('q_tail',max_q['q_tail'])
+
+print('max_p',max_p['p_lag'])
+print('pacf_std',max_p['pacf_std'])
+print('p_tail',max_p['p_tail'])
+
+acf = acf(df_inpc['ipc_t_diff'].dropna())
+pacf = pacf(df_inpc['ipc_t_diff'].dropna())
+
+plt.figure()
+
+plt.subplot(211)
+plt.bar(np.arange(len(acf)),acf,label = 'acf')
+plt.title('ACF')
+
+plt.subplot(212)
+plt.bar(np.arange(len(pacf)),pacf,label = 'pafc')
+plt.title('PACF')
+plt.show()
+
+# Testing to guess a model using Box the Jenkins strategy.
+guess_model_ipc_t_diff = GuessModel(df_inpc['ipc_t_diff'].dropna())
+print('model_ipc_t_diff',guess_model_ipc_t_diff)
+guess_model_ipc = GuessModel(df_inpc['ipc'].dropna())
+print('model_ipc',guess_model_ipc)
